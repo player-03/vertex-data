682a085a72b40ce279c77dac009451d52a094417
Joseph Cloutier <jkcloutier@gmail.com>
2017-02-27 01:01:17 -0800

    Simplify.

--- com/player03/vertexdata/ComponentArray.hx
+++ com/player03/vertexdata/ComponentArray.hx
@@ -3,7 +3,8 @@ package com.player03.vertexdata;
 /**
  * ComponentArray is the type underlying everything else. This isn't
  * itself a vertex buffer, but it contains all the vertex data, and you
- * can upload it using VertexBuffer3D.uploadFromVector().
+ * can upload it using either VertexBuffer3D.uploadFromVector() or
+ * VertexBuffer3D.uploadFromTypedArray().
  */
 
 #if customComponentArrayType
@@ -15,17 +16,10 @@ package com.player03.vertexdata;
 #elseif flash
 	typedef ComponentArray = flash.Vector<Float>;
 #elseif openfl
-	//Float32Array is the preferred format to upload to OpenGL. Note: if
-	//you enable this option, you'll need to use uploadFromFloat32Array()
-	//rather than uploadFromVector(). (Or upload it directly if you're an
-	//advanced user.)
-	#if vertexData_float32Array
-		typedef ComponentArray = openfl.utils.Float32Array;
-	#else
-		typedef ComponentArray = openfl.Vector<Float>;
-	#end
-#elseif (lime && componentArrayIsFloat32Array)
+	//Float32Array is the preferred format to upload to OpenGL.
 	typedef ComponentArray = openfl.utils.Float32Array;
+#elseif lime
+	typedef ComponentArray = lime.utils.Float32Array;
 #else
 	typedef ComponentArray = Array<Float>;
 #end
